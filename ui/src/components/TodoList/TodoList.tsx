a749e6dde2350ab747f25480ff29788566922851
ziggleton <ziggy@devhipsters.co.za>
2020-08-24 18:13:59 +0200

    updated routes, server, todoItems and todoList, updated interfaces and types working update, edit, remove

--- ui/src/components/TodoList/TodoList.tsx
+++ ui/src/components/TodoList/TodoList.tsx
@@ -1,34 +1,89 @@
 import axios from 'axios';
+import { TodoItem } from '../TodoItem/TodoItem';
 import React, { useState, useEffect } from 'react';
 
 export const TodoList: React.FC = () => {
     
-    const [todos, setTodos] = useState<Todo[] | null>(null);
+    const baseUrl: string = 'http://0.0.0.0:4000';
 
-    const baseUrl = 'http://0.0.0.0:4000';
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     useEffect(() => {
-        fetchData();
+        fetchData()
     }, [])
 
-    const toggleComplete = (item: Todo) => {
-        let updatedItems = todos?.map(todo => {
-            return todo === item ? {...todo as {}, complete: !todo.complete} : todo
-        });
-        if (updatedItems != undefined)setTodos(updatedItems);  
+    /**
+     * Toggles todo complete status
+     * @param item 
+     */
+    const completeSwitch: completeSwitch = (item) => {
+        const updatedTodo: Todo = {...item, complete: item.complete ? false : true};  
+        editTodo(updatedTodo);
+        
     };
-    // const createTodo = (item) => {};
-    // const editTodo = (item) => {};
-    // const removeTodo = (item) => {};
+
+    /**
+     * Adds todo to the list
+     * @param item 
+     */
+    const createTodo = (item: Todo) => {};
+
+    /**
+     * Edits todo item
+     * @param item 
+     */
+    const editTodo = (item: Todo) => {
+        axios.post(`${baseUrl}/todos/update/${item._id}`, item)
+            .then(res => {
+                const updatedTodos = todos.map(todo => {
+                    return todo === item ? {...todo, complete: !todo.complete} : todo;
+                });
+                setTodos(updatedTodos);
+            })
+            .catch(error => console.log(error));
+    };
+
+    /**
+     * Removes todo item from the list
+     * @param item 
+     */
+    const removeTodo = (item: Todo) => {
+        axios.delete(`${baseUrl}/todos/${item._id}`)
+            .then(res => {
+                const updatedTodos = todos.filter(todo => todo._id != item._id);
+                setTodos(updatedTodos);
+            })
+            .catch(error => console.log(error));
+    };
+
+    /**
+     * get Data from API
+     */
     const fetchData = () => {
         axios.get(`${baseUrl}/todos`)
-        .then(res => setTodos(res.data))
+        .then(res => {
+            setTodos(res.data);
+        })
         .catch(error => console.log(error));
     }
 
+    /**
+     * Renders Todo list
+     */
+    const renderList = () => {
+        const todolist =  todos.map( todo => {
+            return <TodoItem key={todo._id} todo={todo} completeSwitch={completeSwitch} editTodo={editTodo} removeTodo={removeTodo} />
+        });
+
+        return todolist
+    }
+
     return (
+        
         <div className="todo-list">
-            hello
+            <ul>
+                {renderList()}
+            </ul>
         </div>
     );
 }
